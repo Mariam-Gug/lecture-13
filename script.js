9407cad8e6a717e2627e51139cb16d1accc255cb
Mariam-Gug <89607619+Mariam-Gug@users.noreply.github.com>
2022-04-20 22:43:31 +0400

    პირველი და მეორე

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// პირველი დავალება
+let  user = {
+    firstname: 'giorgi',
+    lastname: 'smith',
+    age: 25,
+   studentstatus: 'active'
+  };
+  
+  console.log(user.studentstatus);
+
+//   მეორე დავალება
+let catNames = ['Snowball', 'Fluffy', 'Luna']
+
+for (let x = 0; x < catNames.length; x++) {
+    console.log(catNames[x]);
+}
+
+for (let x of catNames) {
+    console.log(x);
+}
+
+let y = 0;
+while (y < catNames.length){
+    console.log(catNames[y]);
+    y++;
+}
+
+// დავალება მესამე
+
+let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
